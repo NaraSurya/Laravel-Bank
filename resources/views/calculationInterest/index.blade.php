959a337bd9f5615b34d2a83e317263d6cbc6b578
Nara Surya <ibagungnsd13@gmail.com>
2019-01-08 15:55:43 +0800

    add validate calculation interest

--- resources/views/calculationInterest/index.blade.php
+++ resources/views/calculationInterest/index.blade.php
@@ -13,7 +13,7 @@
                 <div class="col-3">
                     <div class="form-group d-flex">
                         <label for="transactionMonth">Transaction Month</label>
-                        <input type="month" name="transaction_month" class="form-control mx-3" id="transactionMonth">
+                        <input type="month" name="transaction_month" class="form-control mx-3" value="{{ old('transaction_month') }}" id="transactionMonth">
                     </div>
                 </div>
                 <div class="col-3">
@@ -22,6 +22,11 @@
                     </div>
                 </div>
             </div>
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <p class="text-danger">{{$errors->first()}}</p>
+                </div>    
+            @endif
         </form>
         <div class="row mt-3">
             <table class="table table-borderless">
